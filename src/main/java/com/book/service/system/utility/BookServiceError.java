e1418928f3f55f64af08c13774fc9f62e7a7809d
Prerna_Gandhi <prernagandhi97@gmail.com>
2021-05-31 18:40:04 +0530

    Added controller

---
+++ src/main/java/com/book/service/system/utility/BookServiceError.java
@@ -0,0 +1,10 @@
+package com.book.service.system.utility;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BookServiceError {
+    private String errorMsg;
+}
