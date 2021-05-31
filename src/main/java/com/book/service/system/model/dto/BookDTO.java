00e18ef45ecd6f0343731b629a49164e44562329
Prerna_Gandhi <prernagandhi97@gmail.com>
2021-05-31 18:37:02 +0530

    Added book entity and book DTO

---
+++ src/main/java/com/book/service/system/model/dto/BookDTO.java
@@ -0,0 +1,16 @@
+package com.book.service.system.model.dto;
+
+import lombok.Data;
+import lombok.experimental.FieldNameConstants;
+
+import java.math.BigDecimal;
+
+@Data
+@FieldNameConstants
+public class BookDTO {
+    private String title;
+    private String category;
+    private BigDecimal price;
+    private String author;
+    private String publisher;
+}
