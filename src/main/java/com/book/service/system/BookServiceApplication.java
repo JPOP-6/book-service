e8396df89bb0225c035dd9f7f086b475c132076d
Prerna_Gandhi <prernagandhi97@gmail.com>
2021-06-13 14:52:42 +0530

    Setting up eureka client

--- src/main/java/com/book/service/system/BookServiceApplication.java
+++ src/main/java/com/book/service/system/BookServiceApplication.java
@@ -6,6 +6,7 @@ import com.book.service.system.service.BookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -18,6 +19,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 		BookService.class,
 		BookModelMapperImpl.class
 })
+@EnableDiscoveryClient
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
